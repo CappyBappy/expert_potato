b72d78b561143fd48c0e39ace379d7cfa7ed5a74
unknown <cappy_bappy@yahoo.com>
2020-11-25 20:24:55 -0800

    beta version, needs a few bug fixes

--- expert_potato/Bank.java
+++ expert_potato/Bank.java
@@ -1,5 +1,9 @@
 public class Bank {
     public static void main(String[] args) {
-        System.out.println("Hello Bank");
+        Account Cat = new Account("Catherine Larson", "5");
+        Cat.menu();
+        /* I need to fix:
+        1. Layout of menu after options looks crappy
+        2. After you log off, options shouldn't be picked. Program should end. */
     }
 }
